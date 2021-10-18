c608d6b2851cebca468a6e39fa2f6fd5daeffc76
Olha Makoviichuk <olha.makoviichuk@privatbank.ua>
2021-10-18 12:52:56 +0300

    init commit

--- src/com/pb/makoviichuk/hw2/Calculator.java
+++ src/com/pb/makoviichuk/hw2/Calculator.java
@@ -1,5 +1,8 @@
 package com.pb.makoviichuk.hw2;
 
+import javax.xml.transform.Result;
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
         // char grade = args [0]  .charAt (0);
@@ -11,19 +14,22 @@ public class Calculator {
         y = scan.nextInt();
         switch (num) {
             case '+':
-            System.out.println('+')(x+y));
-            break;
+                System.out.println('+')(x + y));
+                break;
             case '-':
-            System.out.println('+')(x-y));
-            break;
+                System.out.println('+')(x - y));
+                break;
             case '*':
-            System.out.println('+')(x*y));
-            break;
+                System.out.println('+')(x * y));
+                break;
             case '/':
-            System.out.println('+')(x/y));
-            break;
-        if (y == 0)
-    {System.out.println("do not divide by zero")
-    break;}
-}
-
+                System.out.println('+')(x / y));
+                break;
+            if (args == '/') ;
+            if (num == 0) {
+                System.out.println("do not divide by zero");
+                break;
+                Result /= num;
+            }
+        }
+    }
--- src/com/pb/makoviichuk/hw2/Interval.java
+++ src/com/pb/makoviichuk/hw2/Interval.java
@@ -1,29 +1,29 @@
 package com.pb.makoviichuk.hw2;
 
+
 public class Interval {
-    System.out.println("Please enter a number between 0 and 100");
+    public static void main(String[] args) {
+        System.out.println("Please enter a number between 0 and 100");
     x = scan.nextInt();
 
     if (x >= 0) {
         System.out.println("[0 -14]");
         if (x <= 14) {
-        System.out.println("[0 -14]");
+            System.out.println("[0 -14]");
         }
-         if (x >=15)
-         if (x <=35) {
-         System.out.println("[15 -35]");
-        }
-         if (x >=36)
-         if (x <=50) {
-         System.out.println("[36 -50]");
-        }
-         if (x >=51)
-         if (x <=100) {
-         System.out.println("[51 -100");
-        }
-         elso (x <0)
-         elso (x >100) {
-         System.out.println("Error.Please enter a number between 0 and 100")
-        }
+        if (x >= 15)
+            if (x <= 35) {
+                System.out.println("[15 -35]");
+            }
+        if (x >= 36)
+            if (x <= 50) {
+                System.out.println("[36 -50]");
+            }
+        if (x >= 51)
+            if (x <= 100) {
+                System.out.println("[51 -100");
+            }
+        if (x < 0)
+            if (x > 100)
+                System.out.println("Error.Please enter a number between 0 and 100");
     }
-}
